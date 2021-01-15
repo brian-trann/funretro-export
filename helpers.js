127231a185081037f8a23c8b9197ac8f91b1af08
brian-trann <70680670+brian-trann@users.noreply.github.com>
2021-01-15 14:09:37 -0800

    add a helper to render a csv string

---
+++ helpers.js
@@ -0,0 +1,33 @@
+/**
+ * renderCsv 
+ * - Args: Array
+ * - Returns: String
+ */
+const renderCsv = (arr) => {
+	const columns = arr.map((el) => {
+		return el['columnTitle'];
+	});
+	let csvString = columns.toString() + '\n';
+	const maxDepth = arr
+		.map((el) => {
+			return el['messages'].length;
+		})
+		.reduce((curr, acc) => {
+			return curr > acc ? curr : acc;
+		});
+	const csvBody = Array(columns.length).fill().map(() => Array(maxDepth));
+	arr.forEach((el, i) => {
+		el['messages'].forEach((message, j) => {
+			csvBody[j][i] = message;
+		});
+	});
+	csvBody.forEach((row) => {
+		csvString += row + '\n';
+	});
+
+	return csvString;
+};
+
+module.exports = {
+	renderCsv : renderCsv
+};
